a9e1af6bcd097635e753f9d8ae8426fdfad1251d
ksm503 <ksm503@gmail.com>
2019-05-26 22:09:07 +0900

    removed unnecessary files

--- src/server/env.js
+++
@@ -1,9 +0,0 @@
-export default {
-  mysql: {
-    host: '**MYSQL ADDRESS**',
-    user: '**MYSQL USERNAME **',
-    password: '**MYSQL PASSWORD **',
-    database: '**MYSQL DB **',
-    todayDataTable: '**MYSQL TABLENAME **'
-  }
-};
