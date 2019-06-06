9fa7ed4b9feceda547813e4cc1c59c80cb236831
ksm503 <ksm503@gmail.com>
2019-06-06 17:19:09 +0900

    EDITED : mongodb query

--- src/server/api/fetch/freeboardPosts.js
+++ src/server/api/fetch/freeboardPosts.js
@@ -1,6 +1,7 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 import env from '../../helpers/serverEnv';
+import dateAndTime from 'date-and-time';
 
 const {
   host,
@@ -20,23 +21,35 @@ const fetchFreeboardPosts = callback => {
   const dbName = `${database}`;
 
   const client = new MongoClient(url, { useNewUrlParser: true });
-
+  // const newTime = dateAndTime.addHours(new Date(), -2);
   client.connect(err => {
     assert.equal(null, err);
 
     const db = client.db(dbName);
     const collection = db.collection(`${freeBoardCollection}`);
     collection
-      .find({})
-      .project({
-        user: 1,
-        _id: 1,
-        text: 1,
-        comments: 1,
-        ip: 1,
-        post_date: 1,
-        admin: 1
-      })
+      .aggregate([
+        // {
+        //   $match: { post_date: { $gt: newTime } }
+        // },
+        {
+          $project: {
+            _id: 1,
+            user: 1,
+            text: 1,
+            ip: 1,
+            post_date: {
+              $dateToString: {
+                format: '%Y.%m.%d %H:%M:%S',
+                date: '$post_date',
+                timezone: 'Asia/Seoul'
+              }
+            },
+            admin: 1,
+            comments: { $size: '$comments' }
+          }
+        }
+      ])
       .sort({ _id: -1 })
 
       .toArray((err, result) => {
