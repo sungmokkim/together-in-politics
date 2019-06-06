1526ad84dc306ff24398a8847448f21d28f40054
ksm503 <ksm503@gmail.com>
2019-06-06 17:18:56 +0900

    CREATED : fetching comments

---
+++ src/server/api/fetch/comments.js
@@ -0,0 +1,69 @@
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+import env from '../../helpers/serverEnv';
+const ObjectId = require('mongodb').ObjectID;
+
+const {
+  host,
+  port,
+  user,
+  mongopw,
+  database,
+  freeBoardCollection
+} = env.mongodb;
+
+const fetchComments = ({ postId }, callback) => {
+  // Connection URL
+
+  const url = `mongodb://${user}:${mongopw}@${host}:${port}`;
+
+  // Database Name
+  const dbName = `${database}`;
+
+  const client = new MongoClient(url, { useNewUrlParser: true });
+
+  client.connect(err => {
+    assert.equal(null, err);
+
+    const db = client.db(dbName);
+    const collection = db.collection(`${freeBoardCollection}`);
+    collection
+      .aggregate([
+        {
+          $match: { _id: ObjectId(postId) }
+        },
+        {
+          $project: {
+            comments: {
+              $map: {
+                input: '$comments',
+                in: {
+                  _id: '$$this._id',
+                  user: '$$this.user',
+                  ip: '$$this.ip',
+                  comment: '$$this.comment',
+                  post_date: {
+                    $dateToString: {
+                      format: '%Y.%m.%d %H:%M:%S',
+                      date: '$$this.post_date',
+                      timezone: 'Asia/Seoul'
+                    }
+                  },
+                  admin: '$$this.admin'
+                }
+              }
+            }
+          }
+        }
+      ])
+
+      .sort({ _id: -1 })
+
+      .toArray((err, result) => {
+        assert.equal(null, err);
+        callback(result);
+      });
+  });
+};
+
+export default fetchComments;
