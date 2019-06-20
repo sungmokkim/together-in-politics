72eeb6204cfa3024f9431349c083371e997c2ee5
ksm503 <ksm503@gmail.com>
2019-06-20 17:52:10 +0900

    REMOVED : unnecessary codes

--- src/shared/pages/FreeBoardPage.js
+++ src/shared/pages/FreeBoardPage.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import FreeBoardSection from '../components/freeboard/sections/FreeBoardSection';
-import { connect } from 'react-redux';
-import { fetchDashboardData, resetCurrentRange } from '../actions/actions';
 
 class FreeBoardPage extends Component {
   componentDidMount() {}
@@ -10,16 +8,6 @@ class FreeBoardPage extends Component {
   }
 }
 
-// const fetchDataFromServerSide = store => {
-//   // const { active } = store.getState()['dashboardManager'];
-//   // return [store.dispatch(fetchDashboardData(active))];
-//   return;
-// };
-
 export default {
-  component: connect(
-    null,
-    { resetCurrentRange, fetchDashboardData }
-  )(FreeBoardPage)
-  // fetchDataFromServerSide
+  component: FreeBoardPage
 };
