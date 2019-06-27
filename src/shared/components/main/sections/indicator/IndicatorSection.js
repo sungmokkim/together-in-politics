e1ed50cb68dd5d6a68241404f179fc99024bd17c
ksm503 <ksm503@gmail.com>
2019-06-27 20:46:39 +0900

    EDITED: refactored codes

--- src/shared/components/main/sections/indicator/IndicatorSection.js
+++ src/shared/components/main/sections/indicator/IndicatorSection.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
 import Indicator from './Indicator';
+import WordCard from './WordCard';
 import { connect } from 'react-redux';
-import { Redirect, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { changeActive, fetchDashboardData } from '../../../../actions/actions';
+import Loading from '../../../common/Loading';
 
 class IndicatorSection extends Component {
-  state = {
-    redirect: false
-  };
-
-  componentDidMount() {
-    this.setState({
-      ...this.state,
-      redirect: false
-    });
-  }
-
-  renderRedirect = () => {
-    // if (this.state.redirect) {
-    //   return <Redirect to='/dashboard' />;
-    // }
-  };
-
   handleChange = index => {
     this.props.changeActive('indicator', index);
     this.props.changeActive('chart', {
@@ -29,77 +14,91 @@ class IndicatorSection extends Component {
       koreanShort: '지표',
       index: 'line'
     });
-    // this.props.fetchDashboardData({
-    //   community: this.props.dashboardManager.active.community,
-    //   indicator: index,
-    //   range: this.props.dashboardManager.active.range
-    // });
   };
+
   getValue = key => {
     const { today } = this.props;
     return today.indicators.length ? today.indicators[0][key] : null;
   };
 
-  render() {
+  renderIndicators = () => {
     const {
       active,
       communities,
       todayIndicators
     } = this.props.dashboardManager;
+
     const { today } = this.props;
+
     return (
-      <section className='section-global'>
-        {this.renderRedirect()}
-        {/* <SectionTitle title='KEY INDICATORS' /> */}
-        <div className='indicator-container'>
-          <Link to='/dashboard'>
-            <Indicator
-              title={todayIndicators.popularity['korean']}
-              index='popularity'
-              value={this.getValue('popularity')}
-              isNumber={true}
-              statusValues={todayIndicators.popularity.statusValues}
-              statusNames={todayIndicators.popularity.statusNames['korean']}
-              statusMarks={todayIndicators.popularity.statusMarks}
-              isFaceEmoji={false}
-              metric='%'
-              activeCommunity={communities[active.community].korean}
-              handleClick={this.handleChange}
-            />
-          </Link>
+      <div className='indicator-container'>
+        <Link to='/dashboard'>
+          <Indicator
+            title={todayIndicators.popularity['korean']}
+            index='popularity'
+            value={this.getValue('popularity')}
+            isNumber={true}
+            statusValues={todayIndicators.popularity.statusValues}
+            statusNames={todayIndicators.popularity.statusNames['korean']}
+            statusMarks={todayIndicators.popularity.statusMarks}
+            isFaceEmoji={false}
+            metric='%'
+            activeCommunity={active.community['korean']}
+            handleClick={this.handleChange}
+          />
+        </Link>
 
-          <Link to='/dashboard'>
-            <Indicator
-              title={todayIndicators.anti_ratio['korean']}
-              index='anti_ratio'
-              value={this.getValue('like_ratio')}
-              isNumber={true}
-              statusValues={todayIndicators.anti_ratio.statusValues}
-              statusNames={todayIndicators.anti_ratio.statusNames['korean']}
-              statusMarks={todayIndicators.anti_ratio.statusMarks}
-              isFaceEmoji={true}
-              facialExpressions={todayIndicators.anti_ratio.facialExpressions}
-              metric='%'
-              activeCommunity={communities[active.community].korean}
-              handleClick={this.handleChange}
-            />
-          </Link>
+        <Link to='/dashboard'>
+          <Indicator
+            title={todayIndicators.anti_ratio['korean']}
+            index='anti_ratio'
+            value={this.getValue('anti_ratio')}
+            isNumber={true}
+            statusValues={todayIndicators.anti_ratio.statusValues}
+            statusNames={todayIndicators.anti_ratio.statusNames['korean']}
+            statusMarks={todayIndicators.anti_ratio.statusMarks}
+            isFaceEmoji={true}
+            facialExpressions={todayIndicators.anti_ratio.facialExpressions}
+            metric='%'
+            activeCommunity={active.community['korean']}
+            handleClick={this.handleChange}
+          />
+        </Link>
+        <Link to='/dashboard'>
+          <Indicator
+            title={todayIndicators.femi_ratio['korean']}
+            index='femi_ratio'
+            value={this.getValue('femi_ratio')}
+            isNumber={true}
+            statusValues={todayIndicators.femi_ratio.statusValues}
+            statusNames={todayIndicators.femi_ratio.statusNames['korean']}
+            statusMarks={todayIndicators.femi_ratio.statusMarks}
+            isFaceEmoji={false}
+            metric='%'
+            activeCommunity={active.community['korean']}
+            handleClick={this.handleChange}
+          />
+        </Link>
 
-          <Link to='/'>
-            <Indicator
-              title='최다빈도 단어'
-              index='word1'
-              value={this.getValue('word1')}
-              isNumber={false}
-              word_value={this.getValue('word1_1')}
-              isFaceEmoji={false}
-              min={0}
-              max={1}
-              activeCommunity={communities[active.community].korean}
-            />
-          </Link>
-        </div>
-      </section>
+        <Link to='/keywords'>
+          <WordCard
+            title='키워드'
+            index='word1'
+            wordArray={this.getValue('words')}
+            isNumber={false}
+            word_value={this.getValue('words')}
+            isFaceEmoji={false}
+            min={0}
+            max={1}
+            activeCommunity={active.community['korean']}
+          />
+        </Link>
+      </div>
+    );
+  };
+  render() {
+    return (
+      <section className='section-global'>{this.renderIndicators()}</section>
     );
   }
 }
