f3a519be0c8e28a47fafa12f7bdd362b64a03828
ksm503 <ksm503@gmail.com>
2019-06-28 15:50:10 +0900

    MOVED : nav display options to separate file

---
+++ src/shared/options/navDisplay.js
@@ -0,0 +1,34 @@
+export const navDisplay = {
+  home: {
+    korean: '메인',
+    index: 'home',
+    linkTo: '/'
+  },
+  dashboard: {
+    korean: '대시보드',
+    index: 'dashboard',
+    linkTo: '/dashboard'
+  },
+  keywords: {
+    korean: '키워드',
+    index: 'keywords',
+    linkTo: '/keywords'
+  },
+
+  rankings: {
+    korean: '랭킹',
+    index: 'rankings',
+    linkTo: '/rankings'
+  },
+  // ,
+  // freeboard: {
+  //   korean: '게시판',
+  //   index: 'freeboard',
+  //   linkTo: '/freeboard'
+  // },
+  about: {
+    korean: 'About',
+    index: 'about',
+    linkTo: '/about'
+  }
+};
