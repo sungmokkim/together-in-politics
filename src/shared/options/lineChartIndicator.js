cf2bc3f867d13ffe6b3eb2e44b4cf88259b29a62
ksm503 <ksm503@gmail.com>
2019-06-30 17:29:51 +0900

    CREATED :new initial state options for line chart

---
+++ src/shared/options/lineChartIndicator.js
@@ -0,0 +1,23 @@
+export const lineChartIndicatorOptions = {
+  anti_ratio: {
+    korean: '적극 거부율',
+    koreanShort: '거부율',
+    checked: true,
+    index: 'anti_ratio',
+    lineColor: '#f75467'
+  },
+  popularity: {
+    korean: '대통령 지분율',
+    koreanShort: '지분율',
+    checked: false,
+    index: 'popularity',
+    lineColor: '#4386f9'
+  },
+  femi_ratio: {
+    korean: '여성 갈등 지수',
+    koreanShort: '여성',
+    checked: true,
+    index: 'femi_ratio',
+    lineColor: '#03c03c'
+  }
+};
