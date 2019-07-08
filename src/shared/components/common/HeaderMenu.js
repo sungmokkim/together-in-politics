739b16a2dac03392a03143d13b70e0b828d6cc4b
ksm503 <ksm503@gmail.com>
2019-07-08 16:02:33 +0900

    ADDED: overlay when menus appear

--- src/shared/components/common/HeaderMenu.js
+++ src/shared/components/common/HeaderMenu.js
@@ -3,22 +3,27 @@ import { connect } from 'react-redux';
 import { Link, NavLink } from 'react-router-dom';
 
 class HeaderMenu extends Component {
-  constructor(props) {
-    super(props);
-  }
+  state = {
+    btnClicked: false,
+    modalDisplay: 'none',
+    menuIsOn: false,
+    portView: null,
+    menuStatus: null
+  };
+
   renderMenus = () => {
     return Object.keys(this.props.site.navDisplay).map((menu, index) => {
       return (
         <NavLink exact to={this.props.site.navDisplay[menu].linkTo} key={menu}>
           <span
-            className={`menu-btn for-desktop ${this.props.menuStatus}`}
+            className={`menu-btn for-desktop ${this.state.menuStatus}`}
             style={{
               opacity: 0,
               animation: `menu-fade-in 0.2s ease-in ${0.1 *
                 (Object.keys(this.props.site.navDisplay).length -
                   (index + 1))}s forwards`
             }}
-            onClick={this.props.handleClosingMenu}
+            onClick={this.handleClosingMenu}
           >
             {this.props.site.navDisplay[menu]['korean']}
           </span>
@@ -27,20 +32,75 @@ class HeaderMenu extends Component {
     });
   };
 
+  handleClickingToggle = () => {
+    if (this.state.menuIsOn) {
+      this.handleClosingMenu();
+    } else {
+      this.setState({
+        ...this.state,
+        menuIsOn: true,
+        menuStatus: 'show',
+        modalDisplay: 'block',
+        btnClicked: true
+      });
+    }
+  };
+
+  handleClosingMenu = () => {
+    this.setState(
+      {
+        ...this.state,
+        menuIsOn: true,
+        menuStatus: 'show',
+        modalDisplay: 'block',
+        btnClicked: false
+      },
+      async () => {
+        await setTimeout(() => {
+          this.setState({
+            ...this.state,
+            menuIsOn: false,
+            menuStatus: null,
+            modalDisplay: 'none',
+            btnClicked: false
+          });
+        }, 300);
+      }
+    );
+  };
+
   render() {
     return (
       <React.Fragment>
+        <div
+          className={
+            //render different animation depending on the current state
+            this.state.btnClicked ? 'opacity-fade-in' : 'opacity-fade-out'
+          }
+          style={{
+            background: 'rgba(0, 0, 0, 0.5)',
+            position: 'fixed',
+            zIndex: '2',
+            top: '0',
+            left: '0',
+            right: '0',
+            bottom: '0',
+            // only display dark overlay when button is clicked
+            display: this.state.modalDisplay
+          }}
+          onClick={this.handleClosingMenu}
+        />
         <div className='menu-wrapper'>
           <div className='menu-toggler-container'>
             <span className='menu-toggler for-mobile'>
               <i
-                className={this.props.menuIsOn ? 'fas fa-times' : 'fas fa-bars'}
+                className={this.state.menuIsOn ? 'fas fa-times' : 'fas fa-bars'}
                 style={{ fontSize: '4rem', color: 'white' }}
-                onClick={this.props.handleClickingToggle}
+                onClick={this.handleClickingToggle}
               />
             </span>
           </div>
-          <div className='nav-menu-container'>{this.renderMenus()}</div>
+          <div className={`nav-menu-container `}>{this.renderMenus()}</div>
         </div>
       </React.Fragment>
     );
