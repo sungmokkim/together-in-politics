dc17cb5150784e85a26198f0370da22e788aec62
sungmokkim <sungmok.kim.dev@gmail.com>
2020-08-07 01:33:23 +0900

    heroku

--- src/shared/clientEnv.js
+++ src/shared/clientEnv.js
@@ -1,3 +1,3 @@
-export const clientFetchingReference = `0.0.0.0:${process.env.PORT || 5000}`;
-export const protocol = 'http';
+export const clientFetchingReference = 'https://together-in-politics.herokuapp.com';
+export const protocol = 'https';
 export const ga = '';
